8cbd5ef8b8f51a6f84683f835c378ad60bbc1b81
Shubham Vernekar <shubhamvernekar@192.168.1.4>
2023-10-23 00:16:28 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # todo-cli-go
 cli for https://github.com/shubhamvernekar/go-todo-api
+
+go build ./cmd/todo
---
+++ cmd/todo/config.go
@@ -0,0 +1,12 @@
+package main
+
+var (
+	BASE_URL = "http://localhost"
+	PORT     = ":3000"
+	GETALL   = "/tasks"
+	GET      = "/task"
+	POST     = "/task"
+	PUT      = "/task"
+	DELETE   = "/task"
+	MARKDONE = "/task/markDone"
+)
